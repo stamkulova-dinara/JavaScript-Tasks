b7c1edede3466cbfa612122786f73cfc764b9d57
Dinara Stamkulova <dinarastamkulova@macs-MacBook-Air.local>
2024-04-24 09:03:07 +0600

    sort colors

--- javascript.js
+++ javascript.js
@@ -1,12 +1,31 @@
-function singleNumber(nums) {
-    let result = 0;
+//Single number
+// function singleNumber(nums) {
+//     let result = 0;
 
-    nums.forEach(element => {
-        result = result ^ element
-    });
+//     nums.forEach(element => {
+//         result = result ^ element
+//     });
 
-    console.log(result)
-    return result;
-};
+//     console.log(result)
+//     return result;
+// };
 
-singleNumber([4,1,2,1,2])
+// singleNumber([4,1,2,1,2])
+
+// Sort Colors
+const sortColors = (nums) => {
+    let count = [0, 0, 0]
+
+    for(let num of nums) count[num]++
+
+    let index = 0
+    for(let i = 0; i<count.length; i++){
+        while(count[i] > 0) {
+            nums[index] = i;
+            count[i]--;
+            index++;
+        }
+    }
+}
+
+console.log(sortColors([2,0,2,1,1,0]))
